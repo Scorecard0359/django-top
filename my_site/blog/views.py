ad39dfaca7e1a974512476dc02a297a814ff2f92
Scorecard0359 <135143366+Scorecard0359@users.noreply.github.com>
2026-06-22 21:30:33 +0700

    Присвоены views для блога и добавлены к urls

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -2,4 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Главная страница")
+    return HttpResponse("""
+        <h1>Главная страница</h1>
+        <p><a href='/post'>Список постов</a></p>
+    """)
+
+def post_list(request):
+    return HttpResponse("""
+        <h1>Список постов</h1>
+        <p><a href='/post/1'>Первый пост</a></p>
+    """)
+
+def get_post(request, id):
+    return HttpResponse(f"""
+        <h1>Пост {id}</h1>
+        <p>немножечно текст</p>
+        <p><a href="/post/{id}/comments">Комментарии</a></p>
+    """)
+
+def get_post_comments(request, id):
+    return HttpResponse(f"""
+        <h2>Комментарии поста {id}:</h2>
+        <ul>
+            <li>эт конечно замечательно, но не мог бы также описать получение всех ачивок?</li>
+            <li>memento mori</li>
+            <li>Пусто здесь...</li>
+        </ul>
+    """)
--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -3,10 +3,13 @@ from django.urls import path, include, re_path
 from blog import views
 
 blogpatterns = [
-    
+    path('', views.post_list),
+    path('<int:id>', views.get_post),
+    path('<int:id>/comments', views.get_post_comments),
 ]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('post/', include(blogpatterns)),
 ]
