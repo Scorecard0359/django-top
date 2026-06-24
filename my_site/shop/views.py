34f7a0d2bf64e77a543ff9004d4cceb94081d0e4
Scorecard0359 <135143366+Scorecard0359@users.noreply.github.com>
2026-06-24 21:33:16 +0700

    Не забудем о shop

--- my_site/shop/views.py
+++ my_site/shop/views.py
@@ -1,34 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Person:
+
+    def __init__(self, name):
+        self.name = name
+
 def index(request):
-    return HttpResponse("<h1>Главная страница</h1>")
+
+    data = {
+        'array': [4, 3, 86, 24, 46]
+    }
+
+    return render(request, "shop/index.html", context=data)
 
 def about(request):
-    return HttpResponse("О нас")
+    return render(request, "shop/about.html")
 
-def contact(request, name: str, age: int, email: str):
-    return HttpResponse(f"""
-        <h2>Name: {name}</h2>
-        <h2>Age: {age}</h2>
-        <h2>E-mail: {email}</h2>
-    """)
-
-def browser_info(request):
-    return HttpResponse(f"""
-        <p>Схема запроса: {request.scheme}</p>
-        <p>Вес тела (байт): {request.body}</p>
-        <p>IP-адрес клиента: {request.META['REMOTE_ADDR']}</p>
-    """)
-
-def user(request, name="undefined", age=0):
-    return HttpResponse(f"Username: {name} Age: {age}")
-
-def product(request, id):
-    return HttpResponse(f"Список товаров {id}")
-
-def new_products(requests, id):
-    return HttpResponse(f"Новые товары {id}")
-
-def top_products(requests, id):
-    return HttpResponse(f"Популярные продукты {id}")
+def contact(request):
+    return render(request, "shop/contact.html")
