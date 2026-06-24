9f6c4b68ca2fdc131706fb775b24978461bd64bc
Scorecard0359 <135143366+Scorecard0359@users.noreply.github.com>
2026-06-24 21:29:29 +0700

    Созданы шаблоны

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -2,30 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("""
-        <h1>Главная страница</h1>
-        <p><a href='/post'>Список постов</a></p>
-    """)
+    return render(request, 'blog/index.html')
 
 def post_list(request):
-    return HttpResponse("""
-        <h1>Список постов</h1>
-        <p><a href='/post/1'>Первый пост</a></p>
-    """)
+    return render(request, 'blog/list.html')
 
 def get_post(request, id):
-    return HttpResponse(f"""
-        <h1>Пост {id}</h1>
-        <p>немножечно текст</p>
-        <p><a href="/post/{id}/comments">Комментарии</a></p>
-    """)
+    return render(request, 'blog/post.html', context={'id': id})
 
 def get_post_comments(request, id):
-    return HttpResponse(f"""
-        <h2>Комментарии поста {id}:</h2>
-        <ul>
-            <li>эт конечно замечательно, но не мог бы также описать получение всех ачивок?</li>
-            <li>memento mori</li>
-            <li>Пусто здесь...</li>
-        </ul>
-    """)
+    return render(request, 'blog/comments.html', context={'id': id})
